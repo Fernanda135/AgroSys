58c711c69490d2f19a13e86265722acc2189965c
Fernanda Araújo <fernandacomun@gmail.com>
2026-07-13 18:52:40 -0300

    fix: hook useStocks

--- frontend/app/hooks/useStocks.ts
+++ frontend/app/hooks/useStocks.ts
@@ -21,6 +21,55 @@ export function useStocks() {
         }
     }
 
+    async function addStock(data: { product_name: string; category: string; quantity: number; unit_price: number; unit: string; }) {
+            try {
+                const newStock = await stockService.create({
+                    product_name: data.product_name,
+                    category: data.category,
+                    quantity: data.quantity,
+                    unit_price: data.unit_price,
+                    unit: data.unit,
+                });
+                setStocks((prev) => [newStock, ...prev]);
+                return newStock;
+            } catch (err) {
+                console.error(err);
+                setError("Erro ao adicionar item no estoque.");
+                throw err;
+            }
+        }
+    
+        async function updateStock(id: number, data: { product_name: string; category: string; quantity: number; unit_price: number; unit: string; }) {
+            try {
+                const updateStock = await stockService.update(id, {
+                    product_name: data.product_name,
+                    category: data.category,
+                    quantity: data.quantity,
+                    unit_price: data.unit_price,
+                    unit: data.unit,
+                });
+                setStocks((prev) =>
+                    prev.map((todo) => (todo.id === id ? updateStock : todo))
+                );
+                return updateStock;
+            } catch (err) {
+                console.error(err);
+                setError("Erro ao atualizar item do estoque.");
+                throw err;
+            }
+        }
+    
+        async function deleteStock(id: number) {
+            try {
+                await stockService.delete(id);
+                setStocks((prev) => prev.filter((todo) => todo.id !== id));
+            } catch (err) {
+                console.error(err);
+                setError("Erro ao deletar item do estoque.");
+                throw err;
+            }
+        }
+
     useEffect(() => {
         loadStocks();
     }, []);
@@ -48,6 +97,10 @@ export function useStocks() {
         loading,
         error,
         reload: loadStocks,
+
+        addStock,
+        updateStock,
+        deleteStock,
 
         totalProducts,
         totalQuantity,
