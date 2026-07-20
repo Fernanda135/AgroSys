94d4b8e5f2f82dc1c725acba59cda0e566102a41
Fernanda Araújo <fernandacomun@gmail.com>
2026-07-20 08:51:07 -0300

    fix: next.config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,34 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/backend/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
+
+  // Configuração de imagens (se necessário)
+  images: {
+    domains: ['agrosys-l2ho.onrender.com'],
+    // Ou use remotePatterns (recomendado)
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'agrosys-l2ho.onrender.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+
+  // Otimizações
+  reactStrictMode: true,
+  poweredByHeader: false,
+  
 };
 
 export default nextConfig;
